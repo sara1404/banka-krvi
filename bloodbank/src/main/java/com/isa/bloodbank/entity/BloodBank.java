088f953b1a1bc2fa6b04e1a2f9445ec380823d9a
Sandra <sandra.brkic5@gmail.com>
2022-11-03 19:24:05 +0100

    Add administrator

--- bloodbank/src/main/java/com/isa/bloodbank/entity/BloodBank.java
+++ bloodbank/src/main/java/com/isa/bloodbank/entity/BloodBank.java
@@ -1,26 +1,29 @@
 package com.isa.bloodbank.entity;
 
-import lombok.AccessLevel;
-import lombok.Data;
-import lombok.experimental.FieldDefaults;
-
 import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.JoinColumn;
 import javax.persistence.OneToOne;
 
+import lombok.AccessLevel;
+import lombok.Data;
+import lombok.experimental.FieldDefaults;
+
 @Data
 @FieldDefaults(level = AccessLevel.PRIVATE)
 @Entity
 public class BloodBank extends BaseEntity {
-    @Column
-    String name;
-    @OneToOne(cascade = CascadeType.ALL)
-    @JoinColumn(name = "address_id", referencedColumnName = "id")
-    Address address;
-    @Column
-    String description;
-    @Column
-    double averageGrade;
+	@Column
+	String name;
+	@OneToOne(cascade = CascadeType.ALL)
+	@JoinColumn(name = "administrator_id", referencedColumnName = "id")
+	User administrator;
+	@OneToOne(cascade = CascadeType.ALL)
+	@JoinColumn(name = "address_id", referencedColumnName = "id")
+	Address address;
+	@Column
+	String description;
+	@Column
+	double averageGrade;
 }
