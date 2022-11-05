b14793f3192197d4e176615df51ab72a546f5554
Ana Vulin <vulin.ra65.2019@uns.ac.rs>
2022-11-05 13:47:05 +0100

    Implemented search by name and city

--- bloodbank/src/main/java/com/isa/bloodbank/controller/BloodBankController.java
+++ bloodbank/src/main/java/com/isa/bloodbank/controller/BloodBankController.java
@@ -4,15 +4,13 @@ import com.isa.bloodbank.entity.BloodBank;
 import com.isa.bloodbank.service.BloodBankService;
 
 import javax.validation.Valid;
+import javax.websocket.server.PathParam;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PatchMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/bloodbank")
@@ -28,6 +26,16 @@ public class BloodBankController {
 	@PatchMapping("/update/")
 	private ResponseEntity<BloodBank> updateBloodBank(@Valid @RequestBody final BloodBank bloodBank) {
 		return ResponseEntity.ok(bloodBankService.update(bloodBank));
+	}
+
+	@GetMapping("/findAll")
+	public ResponseEntity<List<BloodBank>> findAll() {
+		return ResponseEntity.ok(bloodBankService.findAll());
+	}
+
+	@GetMapping("/search")
+	public ResponseEntity<List<BloodBank>> search(@RequestParam String name, @RequestParam String city) {
+		return ResponseEntity.ok(bloodBankService.search(name.trim(), city.trim()));
 	}
 
 }
--- bloodbank/src/main/java/com/isa/bloodbank/service/BloodBankService.java
+++ bloodbank/src/main/java/com/isa/bloodbank/service/BloodBankService.java
@@ -1,9 +1,11 @@
 package com.isa.bloodbank.service;
 
 import com.isa.bloodbank.entity.BloodBank;
+import com.isa.bloodbank.entity.User;
 import com.isa.bloodbank.exception.UserNotFoundException;
 import com.isa.bloodbank.repository.BloodBankRepository;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -31,4 +33,41 @@ public class BloodBankService {
 		findById(bloodBank.getId());
 		return bloodBankRepository.save(bloodBank);
 	}
+
+	public List<BloodBank> search(final String name, final String city) {
+		if (!name.equals("") && !city.equals("")) return searchByNameAndCity(name, city);
+		else if (!name.equals("")) return searchByName(name);
+		else if (!city.equals("")) return searchByCity(city);
+		else return bloodBankRepository.findAll();
+	}
+
+	private List<BloodBank> searchByNameAndCity(final String name, final String city){
+		final List<BloodBank> bloodBanks = new ArrayList<BloodBank>();
+		for (final BloodBank bloodBank : bloodBankRepository.findAll()) {
+			if (bloodBank.getName().toLowerCase().startsWith(name.toLowerCase()) && bloodBank.getAddress().getCity().toLowerCase().startsWith(city.toLowerCase())) {
+				bloodBanks.add(bloodBank);
+			}
+		}
+		return bloodBanks;
+	}
+	private List<BloodBank> searchByName(final String name){
+		final List<BloodBank> bloodBanks = new ArrayList<BloodBank>();
+		for (final BloodBank bloodBank : bloodBankRepository.findAll()) {
+			if (bloodBank.getName().toLowerCase().startsWith(name.toLowerCase())) {
+				bloodBanks.add(bloodBank);
+			}
+		}
+		return bloodBanks;
+	}
+
+	private List<BloodBank> searchByCity(final String city){
+		final List<BloodBank> bloodBanks = new ArrayList<BloodBank>();
+		for (final BloodBank bloodBank : bloodBankRepository.findAll()) {
+			if (bloodBank.getAddress().getCity().toLowerCase().startsWith(city.toLowerCase())) {
+				bloodBanks.add(bloodBank);
+			}
+		}
+		return bloodBanks;
+	}
+
 }
