38d4850c5c42a612844038d6b35416a110ee911f
Sandra <sandra.brkic5@gmail.com>
2022-11-06 19:02:39 +0100

    Component for admins table

--- public-app/src/app/admin-profile/admin-profile.module.ts
+++ public-app/src/app/admin-profile/admin-profile.module.ts
@@ -4,20 +4,24 @@ import { AdminProfileComponent } from './admin-profile/admin-profile.component';
 import {MatFormFieldModule} from '@angular/material/form-field';
 import {MatInputModule} from '@angular/material/input';
 import {MatButtonModule} from '@angular/material/button';
+import {MatTableModule} from '@angular/material/table';
 import { BloodbankInfoComponent } from './bloodbank-info/bloodbank-info.component';
+import { OtherAdminsComponent } from './other-admins/other-admins.component';
 
 
 
 @NgModule({
   declarations: [
     AdminProfileComponent,
-    BloodbankInfoComponent
+    BloodbankInfoComponent,
+    OtherAdminsComponent
   ],
   imports: [
     CommonModule,
     MatFormFieldModule,
     MatInputModule,
-    MatButtonModule
+    MatButtonModule,
+    MatTableModule
   ]
 })
 export class AdminProfileModule { }
---
+++ public-app/src/app/admin-profile/other-admins/other-admins.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-other-admins',
+  templateUrl: './other-admins.component.html',
+  styleUrls: ['./other-admins.component.scss']
+})
+export class OtherAdminsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
