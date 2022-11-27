644bc8782bcfbd5c231ab3a199c2e6f1556c259a
Sandra <sandra.brkic5@gmail.com>
2022-11-27 09:00:33 +0100

    Set condition

--- public-app/src/app/views/users/examination/examination.component.ts
+++ public-app/src/app/views/users/examination/examination.component.ts
@@ -1,3 +1,4 @@
+import { ThisReceiver } from '@angular/compiler';
 import { Component, Inject, Input, OnInit } from '@angular/core';
 import { MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { IUser } from 'src/app/model/User';
@@ -17,6 +18,16 @@ export class ExaminationComponent implements OnInit {
   ngOnInit(): void {
     console.log(this._data.user.id)
     this.userService.getSurveyForUser(this._data.user.id).subscribe(data=>{this.userSurvey = data;});
+  }
+
+  checkSurvey(): boolean{
+    if(this.userSurvey?.weight >= 50 && this.userSurvey?.fluSymptoms == false && this.userSurvey?.skinIrritations == false
+      && this.userSurvey?.abnormalBloodPressure == false && this.userSurvey?.tookAntibiotics == false 
+      && this.userSurvey?.onPeriod == false && this.userSurvey?.dentistIntervention == false 
+      && this.userSurvey?.piercingOrTattoo == false){
+        return true;
+      }
+      return false;
   }
 
 }
