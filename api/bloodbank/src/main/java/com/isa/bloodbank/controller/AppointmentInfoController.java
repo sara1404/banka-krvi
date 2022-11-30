6de79551ed9ed1dcca657c2f533b718696c70d58
Sandra <sandra.brkic5@gmail.com>
2022-11-30 15:26:11 +0100

    Create endpoint

--- api/bloodbank/src/main/java/com/isa/bloodbank/controller/AppointmentInfoController.java
+++ api/bloodbank/src/main/java/com/isa/bloodbank/controller/AppointmentInfoController.java
@@ -1,10 +1,15 @@
 package com.isa.bloodbank.controller;
 
+import com.isa.bloodbank.dto.AppointmentInfoDto;
+import com.isa.bloodbank.dto.BloodBankDto;
 import com.isa.bloodbank.entity.AppointmentInfo;
 import com.isa.bloodbank.service.AppointmentInfoService;
 import com.isa.bloodbank.service.AppointmentService;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,4 +18,9 @@ import org.springframework.web.bind.annotation.RestController;
 public class AppointmentInfoController {
 	@Autowired
 	private AppointmentInfoService appointmentInfoService;
+
+	@PostMapping("/create")
+	public ResponseEntity<AppointmentInfoDto> create( @RequestBody final AppointmentInfoDto appointmentInfoDto){
+		return ResponseEntity.ok(appointmentInfoService.create(appointmentInfoDto));
+	}
 }
