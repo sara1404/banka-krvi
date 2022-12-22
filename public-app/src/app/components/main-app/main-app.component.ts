bc0ea9d676a738b44c83f9ef1123e1924d7f468a
Sandra <sandra.brkic5@gmail.com>
2022-12-22 14:12:03 +0100

    Remover efresh

--- public-app/src/app/components/main-app/main-app.component.ts
+++ public-app/src/app/components/main-app/main-app.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-main-app',
@@ -8,8 +8,13 @@ import { Component, OnInit } from '@angular/core';
 export class MainAppComponent implements OnInit {
 
   constructor() { }
-
+  isLoggedin: boolean
   ngOnInit(): void {
+  }
+
+  login(event: any){
+    this.isLoggedin = event
+    console.log(this.isLoggedin)
   }
 
 }
