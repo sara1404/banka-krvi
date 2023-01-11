7f80ed620e450d288f852093cc564e5b5141b998
Sandra <sandra.brkic5@gmail.com>
2023-01-11 09:39:54 +0100

    Remove excessive

--- api/bloodbank/src/test/java/com/isa/bloodbank/AppoitntmentAdminsTestTest.java
+++ api/bloodbank/src/test/java/com/isa/bloodbank/AppoitntmentAdminsTestTest.java
@@ -26,55 +26,6 @@ public class AppoitntmentAdminsTestTest {
 	@Autowired
 	private AppointmentService appointmentService;
 
-	/*
-	@Test(expected = PessimisticLockingFailureException.class)
-	public void testPessimisticLockingScenario() throws Throwable {
-		final LocalDateTime start = LocalDateTime.now();
-		final Long id = Long.valueOf(16);
-		final ExecutorService executor = Executors.newFixedThreadPool(2);
-		executor.submit(new Runnable() {
-
-			@Override
-			public void run() {
-				System.out.println("Startovan Thread 1");
-				appointmentService.findAvailableMedicalStaff(id, start, 10); // izvrsavanje transakcione metode traje oko 200 milisekundi
-			}
-		});
-		final Future<?> future2 = executor.submit(new Runnable() {
-
-			@Override
-			public void run() {
-				System.out.println("Startovan Thread 2");
-				try {
-					Thread.sleep(150);
-				} catch (final InterruptedException e) {
-					System.out.println("kad udje u drugi");
-				}*/// otprilike 150 milisekundi posle prvog threada krece da se izvrsava drugi
-	/*
-	 * Drugi thread pokusava da izvrsi transakcionu metodu findOneById dok se prvo izvrsavanje iz prvog threada jos nije zavrsilo.
-	 * Metoda je oznacena sa NO_WAIT, sto znaci da drugi thread nece cekati da prvi thread zavrsi sa izvrsavanjem metode vec ce odmah dobiti PessimisticLockingFailureException uz poruke u logu:
-	 * [pool-1-thread-2] o.h.engine.jdbc.spi.SqlExceptionHelper : SQL Error: 0, SQLState: 55P03
-	 * [pool-1-thread-2] o.h.engine.jdbc.spi.SqlExceptionHelper : ERROR: could not obtain lock on row in relation "product"
-	 * Prema Postgres dokumentaciji https://www.postgresql.org/docs/9.3/errcodes-appendix.html, kod 55P03 oznacava lock_not_available
-	 */
-	//System.out.println("poziva drugi servis");
-				/*appointmentService.findAvailableMedicalStaff(id, start, 10);
-				;
-			}
-		});
-		try {
-			//System.out.println("prodje");
-			future2.get(); // podize ExecutionException za bilo koji izuzetak iz drugog child threada
-		} catch (final ExecutionException e) {
-			System.out.println("Exception from thread " + e.getCause().getClass()); // u pitanju je bas PessimisticLockingFailureException
-			throw e.getCause();
-		} catch (final InterruptedException e) {
-			System.out.println("uhvati drugi");
-			e.printStackTrace();
-		}
-		executor.shutdown();
-	}*/
-
 	@Test(expected = PessimisticLockingFailureException.class)
 	public void testPessimisticLockingScenarioCreate() throws Throwable {
 		final BloodBank bloodBank = new BloodBank();
