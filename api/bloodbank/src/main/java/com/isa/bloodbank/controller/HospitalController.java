5db6631f539482b9f671c43279f5232e8016be00
Sandra <sandra.brkic5@gmail.com>
2023-01-21 20:11:34 +0100

    Remove excessive

--- api/bloodbank/src/main/java/com/isa/bloodbank/controller/HospitalController.java
+++ api/bloodbank/src/main/java/com/isa/bloodbank/controller/HospitalController.java
@@ -21,7 +21,6 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api")
 public class HospitalController {
-	String api = "[880DA5360C3DE5960B9168CEFDE5C803]";
 
 	@Autowired
 	private HospitalService hospitalService;
